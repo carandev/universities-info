60c3b61f40d969058a4b08b78f673d7996cd3d11
Carlos Gomez <carlosandres0741@gmail.com>
2022-09-12 17:03:43 -0500

    Fix: Create components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Información de universidades</h1>
+  </header>
+);
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 
-const Home = () => (
-  <>
-    <Header/>
-    <main>
-      <h2>Hi, I am Home</h2>
-    </main>
-    <Footer/>
+const Home = () => {
+  const [greet, setGreet] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:8000')
+      .then(response => response.json())
+      .then(json => setGreet(json))
+  }, [])
+
+  return (
+    <>
+      <Header/>
+      <main>
+        <h2>Hi, {greet['Hello']}</h2>
+      </main>
+      <Footer/>
   </>
-);
+  )
+};
 
 export default Home;
