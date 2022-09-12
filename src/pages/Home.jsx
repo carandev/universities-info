7298172d3f2f3d920fd875ae20c183add6643a82
Carlos Gomez <carlosandres0741@gmail.com>
2022-09-12 17:00:11 -0500

    Fix: Clear default styles

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+
+const Home = () => (
+  <>
+    <Header/>
+    <main>
+      <h2>Hi, I am Home</h2>
+    </main>
+    <Footer/>
+  </>
+);
+
+export default Home;
