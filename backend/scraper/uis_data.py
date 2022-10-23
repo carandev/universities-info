178a8e803e74165efa6fe94e643932bb5039972c
Carlos Gomez <carlosandres0741@gmail.com>
2022-10-23 09:37:00 -0500

    Add: UIS scrapper

---
+++ backend/scraper/uis_data.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+from config.db import conn
+from models.university import universities
+
+UIS_URL = "https://uis.edu.co/uis-programas-pregrado-es/"
+
+request = requests.get(UIS_URL)
+
+soup = BeautifulSoup(request.content, 'html.parser')
+h4_tags = soup.find_all('h4')
+
+for tag in h4_tags:
+    carer_name = tag.get_text()
+    carer_url = tag.find('a')['href']
+
+    carer = {'name': carer_name, 'url': carer_url}
